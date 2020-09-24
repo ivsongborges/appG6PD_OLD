03582907f08e8f4b18c4b7f0651a46751f30d046
Ivson <ivsongborges@gmail.com>
2020-09-23 22:53:33 -0300

    Ajustes no Drawer

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -4,33 +4,10 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        child: Column(children: <Widget>[
-
-            Padding(
-              padding: const EdgeInsets.only(top:40),
-            ),
-            Container(
-              child: Text("Menu"),
-            ),
-            Container(
-              child: Text('O que é G6PD'),
-            ),
-            Container(
-              child: Text('Lista de Restritos'),
-            ),
-          ],
-        ),
-      ),
       appBar: AppBar(
-        backgroundColor: Colors.white,
         title: Center(
-          child: SizedBox(
-            width: 100,
-            child: Image.asset("assets/logo.jpg"),
-          ),
+          child: const Text('G6PD'),
         ),
-
         actions: <Widget>[
           Container(
             width: 60,
@@ -44,15 +21,52 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(
-        color: Color(0xFFF2F3F6),
+      body: ListView(
+          children: <Widget> [
+            ListTile(
+              leading: Icon(
+                  Icons.question_answer
+              ),
+            title: Text('Quiz'),
+            ),
+
+            ListTile(
+              leading: Icon(Icons.details),
+              title: Text('Oq ue é G6PD?'),
+              subtitle: Text('è uma doença...'),
+            )
+          ]
+      ),
+
+      drawer: Drawer(
         child: ListView(
-          children: <Widget>[
-            cardItem(),
-            cardItem(),
-            cardItem(),
-            cardItem(),
-            cardItem(),
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'G6PD - Tudo Sobre a deficiência de G6PD',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.question_answer),
+              title: Text('Quiz'),
+              //onPressed: () => {},
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
           ],
         ),
       ),
@@ -60,42 +74,4 @@ class HomePage extends StatelessWidget {
   }
 }
 
-Widget cardItem() {
-  return Card(
-    child: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        const ListTile(
-          leading: CircleAvatar(
-            backgroundImage: NetworkImage(
-                "https://images.freeimages.com/images/large-previews/75e/question-mark-icon-1444378.jpg"),
-          ),
-          title: Text("O que é G6PD?"),
-          trailing: Icon(Icons.more_vert),
-        ),
-        Container(
-          child: Image.asset("assets/post-picture-001.png"),
-        ),
-        Container(
-          padding: EdgeInsets.all(10),
-          child: Text(
-              "A deficiência de desidrogenase de glicose-6-fosfato (G6PD) é um defeito enzimático ligado ao cromossomo X comum nos negros, que pode resultar em hemólise após doença aguda ou ingestão de fármacos oxidantes (incluindo salicilatos e sulfonamidas). "),
-        ),
-        ButtonTheme.bar(
-          child: ButtonBar(
-            children: <Widget>[
-              FlatButton(
-                child: Icon(Icons.favorite),
-                onPressed: () {},
-              ),
-              FlatButton(
-                child: Icon(Icons.share),
-                onPressed: () {},
-              ),
-            ],
-          ),
-        ),
-      ],
-    ),
-  );
-}
+
